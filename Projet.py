1ee1ff8fc0462fee0b4ea986dffa42bb9acfd0e2
Amandinqu <150437889+Amandinqu@users.noreply.github.com>
2023-11-17 11:20:48 +0100

    Update Projet.py

--- Projet.py
+++ Projet.py
@@ -4,31 +4,33 @@ def display_map(m,d):
             print(d[elem], end="") # affiche la valeur associé de la matrice sans retour à la ligne avec "end" 
         print() # retour à la ligne 
 
-m = [[0,0,0,1,1],
-     [0,0,0,0,1],
-     [1,1,0,0,0],
-     [0,0,0,0,0]]
-dico = {0:" ", 1:"#"}  
-print(display_map(m,dico))
-
 def create_perso(depart):
     x,y = depart
     d = {"char": "o"}
     d["x"] = x # si cle "x" n'existe pas, on l'ajoute au diccionaire avec la valeur x 
     d["y"] = y 
     return d
-print(create_perso((0,0)))
-
 
 def display_map_and_char(m,d,p):
     x,y = p
     M = m.copy()#cf td 1 avec la methode copy
-    personne = create_perso(p)
-    M[x][y] = personne[char]
-    map=display_map(M,d)
-    
-t = (0,0)   
-print(display_map_and_char(m,dico,t))
+    for i,ligne in enumerate(M): 
+        for j,valeur in enumerate(ligne): 
+            if i==p["y"] and j==p["x"]: 
+                print(p["char"],end" ") 
+            else: 
+                print(d[valeur],end" ")
+            print()
+
+m = [[0,0,0,1,1],
+     [0,0,0,0,1],
+     [1,1,0,0,0],
+     [0,0,0,0,0]]
+dico = {0:" ", 1:"#"}
+personnage=create_perso((0,0))
+print(display_map(m,dico))
+print(create_perso((0,0)))
+print(display_map_and_char(m,dico,p))
      
     
 
