ac8e2afe4901e8f09e8955ae7750a7b9e735da9d
EwaLobodzinska <150437969+EwaLobodzinska@users.noreply.github.com>
2023-11-18 18:39:08 +0100

    Update Projet.py
    ameliorations 3.1: ne pas sortir de la carte, 3.2: murs infranchissables

--- Projet.py
+++ Projet.py
@@ -17,7 +17,7 @@ def display_map_and_char(m,d,p):
     M = m.copy()#cf td 1 avec la methode copy
     for i,ligne in enumerate(M): 
         for j,valeur in enumerate(ligne): 
-            if i==p["y"] and j==p["x"]: 
+            if i==p["y"] and j==p["x"]: #y correspond aux lignes de la matrice et x aux valeurs de la matrice (sous-liste)
                 print(p["char"],end" ") 
             else: 
                 print(d[valeur],end" ")
@@ -35,6 +35,27 @@ def update_p(letter,p):
             personne["x"] -=1
     return personne
 
+#proposition pour les ameliorations 3.1, 3.2:
+def update_p(lettre,p,m): #changement de cordonnes en dicctionnaire de personnage
+    if lettre in ["z","s","d","q"]:
+        if lettre == "z": 
+            p["y"] -=1 
+            if p["y"] < 0 or m[p["y"]][p["x"]] == 1: #l'indice de "y" ne peut pas etre plus petite que la nombre des lignes de la matrice et si la valeur de matrice des cordonnees de notre x,y est egal a 1, on a #  
+                p["y"] += 1     
+        if lettre == "s":
+            p["y"] +=1 
+            if p["y"] >= len(m) or m[p["y"]][p["x"]] == 1: #l'indice de "y" ne peut pas etre plus grande que la nombre des lignes de la matrice
+                p["y"] -= 1     
+        if lettre == "d":  
+            p["x"] +=1
+            if p["x"] >= len(m[p["y"]]) or m[p["y"]][p["x"]] == 1: #l'indice de "x" ne peut pas etre plus grande que la nombre des colonnes de la matrice
+                p["x"] -= 1      
+        if lettre == "q":
+            p["x"] -=1
+            if p["x"] < 0 or m[p["y"]][p["x"]] == 1: #l'indice de "x" ne peut pas etre plus petite que la nombre des colonnes de la matrice
+                p["x"] += 1               
+    return p
+
 m = [[0,0,0,1,1],
      [0,0,0,0,1],
      [1,1,0,0,0],
@@ -42,15 +63,15 @@ m = [[0,0,0,1,1],
 dico = {0:" ", 1:"#"}
 p=create_perso((0,0))
 
-print(display_map(m,dico))
+print(display_map(m,dico)) #on a none a la fin a cause de print
 print(create_perso((0,0)))
-print(display_map_and_char(m,dico,p))
+display_map_and_char(m,dico,p) #quand on n'utilise pas le print(display_map...) on n'a pas de none
 
 while True:
     lettre = input("Quel deplacement?")
     update = update_p(lettre,p)         
-    print(display_map_and_char(m,dico,update))
-     
+    display_map_and_char(m,dico,update) #quand on n'utilise pas le print(display_map...) on n'a pas de none
+    #2.4 comment arreter?
     
 
     
